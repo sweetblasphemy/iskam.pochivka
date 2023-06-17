2cb9f81abcace0a728e7fbf0d70fe01a497c5194
sweetblasphemy <nik2006@abv.bg>
2023-06-17 10:41:58 +0300

    Add project files.

---
+++ iskam.pochivka/Program.cs
@@ -0,0 +1,40 @@
+﻿
+namespace iskam.pochivka
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+
+            string[] numbers = Console.ReadLine().Split(' ');
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = int.Parse(numbers[i]);
+            }
+
+            Array.Sort(array);
+
+            int[] result = new int[n];
+            int left = 0;
+            int right = n - 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (i < n / 2)
+                {
+                    result[i] = array[left];
+                    left++;
+                }
+                else
+                {
+                    result[i] = array[right];
+                    right--;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
---
+++ iskam.pochivka/wanepg.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iskam.pochivka
+{
+    internal class wanepg
+    {
+        static int[] BalancedArray(int[] arr)
+        {
+            int n = arr.Length;
+            int[] sortedArr = (int[])arr.Clone();
+            Array.Sort(sortedArr);
+
+            int[] result = new int[n];
+
+            int left = 0;
+            int right = n - 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    result[i] = sortedArr[left];
+                    left++;
+                }
+                else
+                {
+                    result[i] = sortedArr[right];
+                    right--;
+                }
+            }
+
+            return result;
+        }
+    }
+}
